e3c5a7cc176fa07beb7a62145a6634f62770a2d7
Adriana Osorio <driosorio@gmail.com>
2022-05-27 12:19:02 -0300

    still trying to fix heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # A user can see all the lists
-  get 'lists', to: 'lists#index'
+  # get 'lists', to: 'lists#index'
 
-  # A user can create a new list
-  get 'lists/new', to: 'lists#new', as: :new_list
-  post 'lists', to: "lists#create"
+  # # A user can create a new list
+  # get 'lists/new', to: 'lists#new', as: :new_list
+  # post 'lists', to: "lists#create"
 
-  # A user can see the details of a given list and its name
-  get 'lists/:id', to: 'lists#show', as: :list
+  # # A user can see the details of a given list and its name
+  # get 'lists/:id', to: 'lists#show', as: :list
 
+  resources :lists, only: [ :index, :create, :show, :new]
   # resources :movies, only: [ :index, :show ]
 
 
